8c1360226d1f6bfb056057bdf8bcb1f18304b5f5
grover7777@yandex.ru <grafmontekrusto>
2023-04-16 20:57:42 +0300

    Add new project

---
+++ app/src/main/java/grover/notepadandshopinglist/app/test/MainActivity.kt
@@ -0,0 +1,11 @@
+package grover.notepadandshopinglist.app.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
