dd4fe5de0c2cd8d49b6b42d81e24bdf122fde70b
grover7777@yandex.ru <grafmontekrusto>
2023-05-01 23:06:55 +0300

    add entities and create table and column

---
+++ app/src/main/java/grover/notepadandshopinglist/app/test/entities/ShoppingListNames.kt
@@ -0,0 +1,28 @@
+package grover.notepadandshopinglist.app.test.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity (tableName = "shopping_list_names")
+data class ShoppingListNames (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "time")
+    val time: String,
+
+    @ColumnInfo(name = "allItemCounter")
+    val allItemCounter: Int,
+
+    @ColumnInfo(name = "checkedItemsCounter")
+    val checkedItemsCounter: Int,
+
+    @ColumnInfo(name = "ItemsIds")
+    val ItemsIds: String,
+
+): java.io.Serializable
