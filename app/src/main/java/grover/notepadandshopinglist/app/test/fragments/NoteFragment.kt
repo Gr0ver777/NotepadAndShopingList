376aeae04cadf2251524d32075e816da2a124ae9
grover7777@yandex.ru <grafmontekrusto>
2023-06-19 13:43:17 +0300

    Create ViewModel class

--- app/src/main/java/grover/notepadandshopinglist/app/test/fragments/NoteFragment.kt
+++ app/src/main/java/grover/notepadandshopinglist/app/test/fragments/NoteFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import grover.notepadandshopinglist.app.test.R
+import grover.notepadandshopinglist.app.test.activities.MainApp
 import grover.notepadandshopinglist.app.test.databinding.FragmentNoteBinding
+import grover.notepadandshopinglist.app.test.db.MainViewModel
 
 
 class NoteFragment : BaseFragment() {
 
     private lateinit var binding : FragmentNoteBinding
+    private val mainViewModel: MainViewModel by activityViewModels{
+        MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).database)
+    }
     override fun onClickNew() {
 
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
