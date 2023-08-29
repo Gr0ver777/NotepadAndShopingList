35c03257aa28a7584e9d5700984e535f33af8da8
grover7777@yandex.ru <grafmontekrusto>
2023-08-29 16:28:05 +0300

    ActivityResultLauncher

--- app/src/main/java/grover/notepadandshopinglist/app/test/activities/NewNoteActivity.kt
+++ app/src/main/java/grover/notepadandshopinglist/app/test/activities/NewNoteActivity.kt
@@ -1,11 +1,13 @@
 package grover.notepadandshopinglist.app.test.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import grover.notepadandshopinglist.app.test.R
 import grover.notepadandshopinglist.app.test.databinding.ActivityNewNoteBinding
+import grover.notepadandshopinglist.app.test.fragments.NoteFragment
 
 class NewNoteActivity : AppCompatActivity() {
 
@@ -25,11 +27,20 @@ class NewNoteActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId == R.id.save_note){
-            finish()
+            setMainResult()
         } else if(item.itemId == android.R.id.home){
             finish()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMainResult(){
+        val i = Intent().apply {
+            putExtra(NoteFragment.TITLE_KEY, binding.edTitle.text.toString())
+            putExtra(NoteFragment.DESC_KEY, binding.edDescription.text.toString())
+        }
+        setResult(RESULT_OK, i)
+        finish()
     }
 
     private fun actionBarSettings(){
